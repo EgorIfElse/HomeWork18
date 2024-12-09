7dacc869680a139b30216744a391f86f7c839681
user <user@WIN-PRBG4ODTMQN>
2024-12-09 18:48:13 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,55 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string input = "}{d(cc[])vvvd(fdbd)}";
+        Stack<char> stack = new Stack<char>();
+
+        foreach (char c in input)
+        {
+            if (c == '{' || c == '[' || c == '(')
+
+            {
+                stack.Push(c);
+            }
+            else if (c == '}' || c == ']' || c == ')')
+            {
+                if (stack.Count == 0)
+                {
+                    Console.WriteLine("Cкобки расставлены некорректно");
+                    return;
+                }
+
+                char open = stack.Pop();
+
+
+                if (!((c == '}' && open == '{') || (c == ']' && open == '[') || (c == ')' && open == '(')))
+                {
+
+                    Console.WriteLine("Скобки расставлены некорректно");
+
+                }
+            }
+        }
+
+        if (stack.Count == 0)
+        {
+            Console.WriteLine("Отлично");
+        }
+        else
+        {
+            Console.WriteLine("Скобки расставлены некорректно");
+        }
+    }
+                
+
+
+
+
+
+
+
+
+
+
+}
